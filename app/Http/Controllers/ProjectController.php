a5573f06ca26359e38c83b4856ad0f053a460a83
ebrahemalaa733-cloud <ebrahemalaa733@gmail.com>
2025-11-27 13:11:45 +0300

    project-type-fix

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -14,7 +14,7 @@ class ProjectController extends Controller
     public function index()
     {
         $projectTypes = Project::all();
-        return apiSuccess('All Projects' , $projects);
+        return apiSuccess('All Projects' , $projectTypes);
     }
 
     /**
