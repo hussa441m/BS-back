6157dc471b892676b6840f83f561a900e6a1997e
kinghussam75 <kinghussam75@gmail.com>
2025-12-25 20:49:41 +0300

    auth - middleware

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+    protected $fillable = [  'is_consultant','experience_start', 'admin_comment','user_id','role_id'];
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\ContactTypeController;
 use App\Http\Controllers\Admin\DocumentTypeController;
 use App\Http\Controllers\Admin\ProjectTypeController;
 use App\Http\Controllers\Admin\ٌRoleController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,15 +15,26 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
-Route::apiResource('/projects', ProjectController::class);
 
-Route::apiResource('/project-types', ProjectTypeController::class );
-Route::apiResource('/document-types', DocumentTypeController::class  );
-Route::apiResource('/contact-types', ContactTypeController::class  );
-Route::apiResource('/account-statuses', AccountStatusController::class  );
-Route::apiResource('/roles', ٌRoleController::class  );
-    
+// Route::middleware('auth:sanctum')->group(function () {
+
+    // Route::middleware('user-type:admin')->group(function () {
+        Route::apiResource('/project-types', ProjectTypeController::class);
+        Route::apiResource('/document-types', DocumentTypeController::class);
+        Route::apiResource('/contact-types', ContactTypeController::class);
+        Route::apiResource('/account-statuses', AccountStatusController::class);
+        Route::apiResource('/roles', ٌRoleController::class);
+    // });
+
+    // Route::middleware('user-type:admin')->group(function () {
+        Route::apiResource('/projects', ProjectController::class);
+        Route::post('logout', [AuthController::class, 'logout']);
+    // });
+// });
+
 
 Route::fallback(function () {
     return apiError("path is incorrect", [
